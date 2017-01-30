219185e0c52f004f29e393e7ffa1074ea7e42b36
rick <rick@coalescentdesign.com>
2017-01-30 05:22:50 -0800

    Turn on Watson for data.

--- server.js
+++ server.js
@@ -12,37 +12,37 @@ app.get('/*', (req, res) => {
   res.sendFile( __dirname + "/build/" + "index.html" )
 })
 
-// Post to Watson
+// Post to Watson.
 app.post('/api', (req, res) => {
   console.log('posted to server');
   const text = req.body.resume + ' ' + req.body.cover
-  const response = {
-    personality: [
-      {
-        name: 'Openness',
-        percentile: .95
-      },
-      {
-        name: 'Kindness',
-        percentile: .80
-      }
-    ]
-  }
-
-  res.send(JSON.stringify(response, null, 2))
-  // personalityInsights.profile({
-  //     text: text,
-  //     consumption_preferences: false
-  //   },
-  //   (err, response) => {
-  //     if (err) {
-  //       console.log('error:', err)
-  //       res.status(500).send(err)
-  //     } else {
-  //       console.log("success")
-  //       res.send(JSON.stringify(response, null, 2))
+  // const response = {
+  //   personality: [
+  //     {
+  //       name: 'Openness',
+  //       percentile: .95
+  //     },
+  //     {
+  //       name: 'Kindness',
+  //       percentile: .80
   //     }
-  //   });
+  //   ]
+  // }
+  // res.send(JSON.stringify(response, null, 2))
+
+  personalityInsights.profile({
+      text: text,
+      consumption_preferences: false
+    },
+    (err, response) => {
+      if (err) {
+        console.log('error:', err)
+        res.status(500).send(err)
+      } else {
+        console.log("success")
+        res.send(JSON.stringify(response, null, 2))
+      }
+    });
 })
 
 // Start the server
